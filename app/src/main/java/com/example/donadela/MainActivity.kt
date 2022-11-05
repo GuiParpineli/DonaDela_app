04bd818d71cebbb170a2d0c676734e6669515bab
Gui Parpineli <guiparpineli@GuiPC>
2022-11-05 10:29:58 -0300

    create second activity

--- app/src/main/java/com/example/donadela/MainActivity.kt
+++ app/src/main/java/com/example/donadela/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.donadela
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.donadela.R.id.button_products
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnProducts = findViewById<Button>(button_products)
+
+        btnProducts.setOnClickListener {
+            val intent = Intent(this, ProductsAndToys::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/donadela/ProductsAndToys.kt
@@ -0,0 +1,12 @@
+package com.example.donadela
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.donadela.R.layout.products_and_toys
+
+class ProductsAndToys : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(products_and_toys)
+    }
+}
